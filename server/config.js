0b8eff7c716dd37562a9a457ca9b6f2ab5ce1045
10thfloor <remember.mackenzie@gmail.com>
2017-01-04 15:00:15 -0800

    Add express history API fallback.

--- server/config.js
+++ server/config.js
@@ -2,9 +2,11 @@ const nconf = require('nconf');
 const config = module.exports = nconf.argv()
 
 .env([
-  'APP_SECRET'
+  'APP_SECRET',
+  'STATIC_PATH'
 ])
 
 .defaults({
+  'STATIC_PATH': '../web.browser/build',
   'HTTPS': (process.env.NODE_ENV === 'production')
 })
--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,16 @@
 import express from 'express'
 import { resolve } from 'path'
 
+import fallback from 'express-history-api-fallback';
+
+import config from '../config'
+
+const root = resolve(process.cwd(), config.get('STATIC_PATH'));
 const app = express();
 
-app.use(express.static((resolve(process.cwd(), '../web.browser/build'))));
+app.use(express.static(root));
+
+app.use(fallback('index.html', { root }));
 
 app.use((req, res, next) => {
   res.status(404).send('Page not found...');
